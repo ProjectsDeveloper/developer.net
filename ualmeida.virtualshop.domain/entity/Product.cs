f8dc317d3ffc1b7c2972864c12a7af8a56b6f7ff
ProjectsDeveloper <projects.developer@outlook.com>
2015-09-23 16:45:23 -0300

    Product Class

---
+++ ualmeida.virtualshop.domain/entity/Product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ualmeida.virtualshop.domain.entity
+{
+    public class Product
+    {
+        public int IdProduct { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public Decimal Price { get; set; }
+        public string Category { get; set; }
+    }
+}
